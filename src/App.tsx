9d8145ac9d015fa3e69bb666e440be95395dce7f
Giovane Santos <giovane98sp@gmail.com>
2023-08-16 16:39:19 -0300

    create search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom"
 
 export const App = () => {
 
   return (
-    <div>
+    <div className="app">
       <h1>Github Finder</h1>
+      <Outlet />
     </div>
   )
 }
---
+++ src/routes/Home.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+import { Search } from "../components/Search"
+import { UserProps } from "../types/user"
+
+export const Home = () => {
+  const [user, setUser] = useState<UserProps | null>(null)
+
+  const loadUser = async (userName: string) => {
+    const res = await fetch(`https://api.github.com/users/${userName}`)
+
+    const data = await res.json()
+
+    console.log(data)
+  }
+
+  return (
+    <div>
+      <Search loadUser={loadUser} />
+    </div>
+  )
+}
